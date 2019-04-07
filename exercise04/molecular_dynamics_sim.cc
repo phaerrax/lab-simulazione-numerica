e780fee6a2cc1f39afd32bdb780d3a5d7d5a4747
Davide Ferracin <dv.ferracin@gmail.com>
2019-04-08 00:48:11 +0200

    [4] Rewrite initialisation methods
    The methods in the 'molecular_dynamics_sim' class that gather the input
    parameters and initialise the configuration were rewritten, using STL
    libraries and possibly generalising the code to an arbitrary number of
    dimensions.
    The methods 'move' and 'force' were tidied up, too, in order to complete
    the initialisation procedure.
    The bit regarding the sampling from a Maxwell-Boltzmann distribution is
    still incomplete; it will require more time.

--- exercise04/molecular_dynamics_sim.cc
+++ exercise04/molecular_dynamics_sim.cc
@@ -1,11 +1,11 @@
 #include "molecular_dynamics_sim.hh"
+#include <string>
+#include <sstream>
+#include "random.hh"
 
-void molecular_dynamics_sim::input(Random & rng)
+void molecular_dynamics_sim::set_init_config(const std::string & input_parameters_file)
 {   
     // It is assumed that the random number generator is already initialised.
-
-    std::string input_parameters_file("input.dat"),
-                initial_config_file("config.0");
 
     double ep, ek, pr, et, vir; // ??
 
@@ -18,157 +18,322 @@ void molecular_dynamics_sim::input(Random & rng)
     std::cout << "Read input parameters from file " << input_parameters_file << "." << std::endl;
     std::ifstream input_parameters(input_parameters_file);
 
-    input_parameters >> temp;
+    input_parameters >> input_temperature;
 
-    input_parameters >> npart;
-    std::cout << "Number of particles = " << npart << std::endl;
+    input_parameters >> n_particles;
+    std::cout << "Number of particles: " << n_particles << "\n";
 
-    input_parameters >> rho;
-    std::cout << "Density of particles = " << rho << std::endl;
-    vol = static_cast<double>(npart)/rho;
-    std::cout << "Volume of the simulation box = " << vol << std::endl;
-    box = pow(vol,1.0/3.0);
-    std::cout << "Edge of the simulation box = " << box << std::endl;
+    input_parameters >> particle_density;
+    std::cout << "Density of particles: " << particle_density << "\n";
 
-    input_parameters >> rcut;
-    input_parameters >> delta;
-    input_parameters >> nstep;
-    input_parameters >> iprint;
+    total_volume = static_cast<double>(n_particles) / particle_density;
+    std::cout << "Volume of the simulation cell: " << total_volume << "\n";
+
+    cell_edge_length = std::pow(total_volume, 1. / 3.);
+    std::cout << "Edge of the simulation cell: " << cell_edge_length << "\n";
+
+    input_parameters >> distance_cutoff;
+    input_parameters >> time_step;
+    input_parameters >> n_steps;
+    input_parameters >> print_steps;
 
     std::cout << "The program integrates Newton equations with the Verlet method, using\n
-                  Time step = " << delta << ",\n
-                  Number of steps = " << nstep << "." << std::endl;
-    input_parameters.close(),
+                  Time step: " << time_step << ",\n
+                  Number of steps: " << n_steps << "." << std::endl;
+
+    input_parameters.close();
+    return;
+}
+
+void molecular_dynamics_sim::initialise_uniform(const std::string & initial_config_file, Random & rng)
+{
+    // Clear the previous configuration (if there is any).
+    position.clear();
 
     // Read the initial configuration from a file.
     std::cout << "Read initial configuration from file " << initial_config_file << "." << std::endl;
     std::ifstream input_config(initial_config_file);
 
-    for(unsigned int i = 0; i < npart; ++i)
+    std::vector<double> new_point;
+    std::string line, coordinate;
+
+    // Read the file line by line.
+    while(std::getline(input_config, line))
     {
-        input_config >> x[i] >> y[i] >> z[i];
-        x[i] = x[i] * box;
-        y[i] = y[i] * box;
-        z[i] = z[i] * box;
+        // Save each line in a new vector.
+        // (The coordinates are given in units of the cell edge length, so
+        // we need to rescale them.)
+        std::istringstream iss(line);
+        while(iss >> coordinate)
+            new_point.push_back(cell_edge_length * std::stod(coordinate));
+
+        // Add the new vector to the list of positions of the particles.
+        position.push_back(new_point);
     }
+    // Maybe check that the length of the 'position' vector actually equals
+    // the variable 'n_particles'.
+
     input_config.close();
 
     // Simulate an initial velocity configuration.
-    std::cout << "Prepare random velocities with center of mass velocity equal to zero " << std::endl << std::endl;
-    double sumv[3] = {0.0, 0.0, 0.0};
-    for(unsigned int i = 0; i < npart; ++i)
-    {
-        vx[i] = rng.Rannyu() - 0.5;
-        vy[i] = rng.Rannyu() - 0.5;
-        vz[i] = rng.Rannyu() - 0.5;
+    std::cout << "Prepare random velocities with center of mass velocity equal to zero." << std::endl;
 
-        sumv[0] += vx[i];
-        sumv[1] += vy[i];
-        sumv[2] += vz[i];
-    }
-    for(unsigned int idim = 0; idim < 3; ++idim)
-        sumv[idim] /= static_cast<double>(npart);
-    double sumv2 = 0.0, fs;
-    for(unsigned int i = 0; i < npart; ++i)
-    {
-        vx[i] = vx[i] - sumv[0];
-        vy[i] = vy[i] - sumv[1];
-        vz[i] = vz[i] - sumv[2];
-        sumv2 += vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i];
-    }
-    sumv2 /= static_cast<double>(npart);
+    unsigned int n_coordinates = position.begin()->size();
+    // The number of dimensions, i.e. of coordinates of each velocity vector.
 
-    fs = sqrt(3 * temp / sumv2);   // fs = velocity scale factor 
-    for(unsigned int i = 0; i < npart; ++i)
-    {
-        vx[i] *= fs;
-        vy[i] *= fs;
-        vz[i] *= fs;
+    std::vector<double> sum(n_coordinates, 0.);
+    for(auto & particle_velocity : velocity)
+        for(unsigned int i = 0; i < n_coordinates; ++i)
+        {
+            // Generate uniformly distributed velocities in [-0.5, 0.5).
+            particle_velocity[i] = rng.Rannyu() - 0.5;
+            // The velocities will be translated such that the velocity of
+            // the centre of mass is zero. For this, we need the sum of
+            // the velocities.
+            sum[i] += particle_velocity[i];
+        }
 
-        xold[i] = x[i] - vx[i] * delta;
-        yold[i] = y[i] - vy[i] * delta;
-        zold[i] = z[i] - vz[i] * delta;
+    for(unsigned int i = 0; i < n_coordinates; ++i)
+        sum[i] /= n_particles;
+
+    double sum_sq_velocities;
+    for(auto & particle_velocity : velocity)
+        for(unsigned int i = 0; i < n_coordinates; ++i)
+        {
+            // Subtract from each velocity the centre of mass velocity
+            // calculated above.
+            particle_velocity[i] -= sum[i];
+            // Compute the sum of the squares of the velocity of each
+            // particle.
+            sum_sq_velocities += std::inner_product(particle_velocity.begin(), particle_velocity.end(), particle_velocity.begin(), 0.);
+        }
+
+    // Divide by the number of particles to obtain the mean square velocity.
+    sum_sq_velocities /= n_particles;
+
+    // Rescale all the velocities in order to match the input temperature
+    // given before.
+    double scale_factor = std::sqrt(3. * input_temperature / sum_sq_velocities);
+    for(auto & particle_velocity : velocity)
+        for(unsigned int i = 0; i < n_coordinates; ++i)
+        {
+            particle_velocity[i] *= scale_factor;
+            // From the initial position and the initial velocity, extrapolate
+            // a value for the "pre-initial" position.
+            old_position[i] = position[i] - particle_velocity[i] * time_step;
     }
+
+    return;
+}
+
+void molecular_dynamics_sim::initialise_maxwellboltzmann(const std::string & initial_config_file, Random & rng)
+{
+    // Clear the previous configuration (if there is any).
+    position.clear();
+
+    // Read the initial configuration from a file.
+    std::cout << "Read initial configuration from file " << initial_config_file << "." << std::endl;
+    std::ifstream input_config(initial_config_file);
+
+    std::vector<double> new_point;
+    std::string line, coordinate;
+
+    // Read the file line by line.
+    while(std::getline(input_config, line))
+    {
+        // Save each line in a new vector.
+        // (The coordinates are given in units of the cell edge length, so
+        // we need to rescale them.)
+        std::istringstream iss(line);
+        while(iss >> coordinate)
+            new_point.push_back(cell_edge_length * std::stod(coordinate));
+
+        // Add the new vector to the list of positions of the particles.
+        position.push_back(new_point);
+    }
+    // Maybe check that the length of the 'position' vector actually equals
+    // the variable 'n_particles'.
+
+    input_config.close();
+
+    // Simulate an initial velocity configuration.
+    std::cout << "Sample random velocities from a Maxwell-Boltzmann distribution." << std::endl;
+
+    unsigned int n_coordinates = position.begin()->size();
+    // The number of dimensions, i.e. of coordinates of each velocity vector.
+
+    // Sampling...
+    
+    for(unsigned int i = 0; i < n_particles; ++i)
+        for(unsigned int d = 0; d < n_coordinates; ++d)
+            // From the initial position and the initial velocity, extrapolate
+            // a value for the "pre-initial" position.
+            old_position[i][d] = position[i][d] - velocity[i][d] * time_step;
+
+    return;
+}
+
+void molecular_dynamics_sim::initialise_from_file(const std::string & initial_config_file, const std::string & pre_initial_config_file)
+{
+    // Clear the previous configuration (if there is any).
+    position.clear();
+    old_position.clear();
+
+    // Read the initial configuration from a file.
+    std::cout << "Read initial configuration from file " << initial_config_file << "." << std::endl;
+    std::ifstream input_config(initial_config_file);
+
+    std::vector<double> new_point;
+    std::string line, coordinate;
+
+    // Read the file line by line.
+    while(std::getline(input_config, line))
+    {
+        // Save each line in a new vector.
+        // (The coordinates are given in units of the cell edge length, so
+        // we need to rescale them.)
+        std::istringstream iss(line);
+        while(iss >> coordinate)
+            new_point.push_back(cell_edge_length * std::stod(coordinate));
+
+        // Add the new vector to the list of positions of the particles.
+        position.push_back(new_point);
+    }
+    // Maybe check that the length of the 'position' vector actually equals
+    // the variable 'n_particles'.
+
+    input_config.close();
+
+    // Same as above, but with the pre-initial configuration.
+    std::cout << "Read pre-initial configuration from file " << pre_initial_config_file << "." << std::endl;
+    input_config.open(pre_initial_config_file);
+
+    // Read the file line by line.
+    while(std::getline(input_config, line))
+    {
+        // Save each line in a new vector.
+        // (The coordinates are given in units of the cell edge length, so
+        // we need to rescale them.)
+        std::istringstream iss(line);
+        while(iss >> coordinate)
+            new_point.push_back(cell_edge_length * std::stod(coordinate));
+
+        // Add the new vector to the list of positions of the particles.
+        old_position.push_back(new_point);
+    }
+
+    input_config.close();
+
+    // Calculate the initial velocity configuration.
+    // 1. Compute the position at the first step using the Verlet algorithm,
+    //    obtaining the current velocity too.
+    // 2. Compute the temperature and compare it with the given one.
+    // 3. Rescale the velocities to match the current temperature with the
+    //    given one.
+    // 4. Correct the old positions, using the newly computed velocities.
+
+    // Before moving, save the original positions in a new vector.
+    std::vector<std::vector<double>> original_init_position(position);
+
+    move();
+    double sum_sq_velocities;
+    for(auto & particle_velocity : velocity)
+        // Compute the sum of the squares of the velocity of each particle.
+        sum_sq_velocities += std::inner_product(particle_velocity.begin(), particle_velocity.end(), particle_velocity.begin(), 0.);
+
+    // Divide by the number of particles to obtain the mean square velocity.
+    sum_sq_velocities /= n_particles;
+
+    // Rescale all the velocities in order to match the input temperature
+    // given before.
+    double scale_factor = std::sqrt(3. * input_temperature / sum_sq_velocities);
+    for(unsigned int i = 0; i < n_particles; ++i) 
+        for(unsigned int d = 0; d < n_coordinates; ++d)
+        {
+            velocity[i][d] *= scale_factor;
+
+            // From the (original) initial position and the initial
+            // velocity, extrapolate a value for the "pre-initial" position.
+            old_position[i][d] = original_init_position[i][d] - velocity[i][d] * time_step;
+        }
+    // Restore the original values (as read from the file) of the initial
+    // positions.
+    position = original_init_position;
+
     return;
 }
 
 void molecular_dynamics_sim::move(void)
-{ //Move particles with Verlet algorithm
-    double xnew, ynew, znew, fx[npart], fy[npart], fz[npart];
-
-    for(unsigned int i = 0; i < npart; ++i)
+{
+    // Move particles using Verlet algorithm.
+    std::vector<std::vector<double>> forces(n_particles);
+    unsigned int n_coordinates = position.begin()->size();
+    std::vector<double> new_position(n_coordinates);
+    for(unsigned int i = 0; i < n_particles; ++i)
     { 
-        // Calculate the force acting on particle i.
-        fx[i] = force(i,0);
-        fy[i] = force(i,1);
-        fz[i] = force(i,2);
-    }
+        for(unsigned int d = 0; d < n_coordinates; ++d)
+        {
+            // Verlet integration scheme.
+            new_position[d] = 
+                quotient(
+                        2. * position[i][d] - old_position[i][d] + force(i, d) * std::pow(time_step, 2)
+                        );
+            velocity[i][d] = quotient(new_position[d] - old_position[i][d]) / (2. * time_step);
+        }
 
-    for(unsigned int i = 0; i < npart; ++i)
-    { 
-        // Verlet integration scheme
-        xnew = quotient( 2.0 * x[i] - xold[i] + fx[i] * pow(delta,2) );
-        ynew = quotient( 2.0 * y[i] - yold[i] + fy[i] * pow(delta,2) );
-        znew = quotient( 2.0 * z[i] - zold[i] + fz[i] * pow(delta,2) );
-
-        vx[i] = quotient(xnew - xold[i])/(2.0 * delta);
-        vy[i] = quotient(ynew - yold[i])/(2.0 * delta);
-        vz[i] = quotient(znew - zold[i])/(2.0 * delta);
-
-        xold[i] = x[i];
-        yold[i] = y[i];
-        zold[i] = z[i];
-
-        x[i] = xnew;
-        y[i] = ynew;
-        z[i] = znew;
+        // Move current position to old position, and new to current.
+        old_position[i] = position[i];
+        position[i] = new_position;
     }
     return;
 }
 
-double molecular_dynamics_sim::force(int ip, int idir)
-{ //Compute forces as -Grad_ip V(r)
-    double f=0.0;
-    double dvec[3], dr;
+double molecular_dynamics_sim::force(unsigned int this_particle, unsigned int dir)
+{
+    // Compute forces as -grad V(r)
+    double f(0), distance;
+    unsigned int n_coordinates = position.begin()->size();
+    std::vector<double> displacement(n_coordinates);
 
-    for (unsigned int i = 0; i < npart; ++i)
+    for(unsigned int i = 0; i < n_particles; ++i)
     {
-        if(i != ip)
+        if(i != this_particle)
         {
-            dvec[0] = quotient( x[ip] - x[i] );  // distance ip-i in pbc
-            dvec[1] = quotient( y[ip] - y[i] );
-            dvec[2] = quotient( z[ip] - z[i] );
-
-            dr = dvec[0]*dvec[0] + dvec[1]*dvec[1] + dvec[2]*dvec[2];
-            dr = sqrt(dr);
-            if(dr < rcut)
+            for(unsigned int d = 0; d < n_coordinates; ++d)
+                displacement[d] = quotient(position[this_particle][d] - position[i][d]);
+            distance = std::sqrt(std::inner_product(displacement.begin(), displacement.end(), displacement.begin(), 0.));
+            if(dr < distance_cutoff)
             {
-                f += dvec[idir] * (48.0/pow(dr,14) - 24.0/pow(dr,8)); // -Grad_ip V(r)
+                f += displacement[d] * (48. * std::pow(distance, -14) - 24. * std::pow(distance, -8));
             }
         }
     }
+
     return f;
 }
 
 void molecular_dynamics_sim::measure()
-{ //Properties measurement
-    int bin;
-    double v, t, vij;
+{
+    // Compute thermodynamical quantities of the system, and append the
+    // results to the given files.
+    double potential_energy(0),
+           kinetic_energy(0),
+           vij; // ??
     double dx, dy, dz, dr;
     std::ofstream Epot, Ekin, Etot, Temp;
 
-    Epot.open("output_epot.dat",ios::app);
-    Ekin.open("output_ekin.dat",ios::app);
-    Temp.open("output_temp.dat",ios::app);
-    Etot.open("output_etot.dat",ios::app);
+    Epot.open("output_epot.dat", std::ios::app);
+    Ekin.open("output_ekin.dat", std::ios::app);
+    Temp.open("output_temp.dat", std::ios::app);
+    Etot.open("output_etot.dat", std::ios::app);
 
     v = 0.0; //reset observables
     t = 0.0;
 
     //cycle over pairs of particles
-    for(unsigned int i = 0; i < npart-1; ++i)
+    for(unsigned int i = 0; i < n_particles-1; ++i)
     {
-        for(int j=i+1; j<npart; ++j)
+        for(int j=i+1; j<n_particles; ++j)
         {
             dx = quotient( x[i] - x[j] );
             dy = quotient( y[i] - y[j] );
@@ -176,7 +341,7 @@ void molecular_dynamics_sim::measure()
 
             dr = dx*dx + dy*dy + dz*dz;
             dr = sqrt(dr);
-            if(dr < rcut)
+            if(dr < distance_cutoff)
             {
                 vij = 4.0/pow(dr,12) - 4.0/pow(dr,6);
                 //Potential energy
@@ -186,13 +351,13 @@ void molecular_dynamics_sim::measure()
     }
 
     //Kinetic energy
-    for(unsigned int i = 0; i < npart; ++i)
+    for(unsigned int i = 0; i < n_particles; ++i)
         t += 0.5 * (vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i]);
 
-    stima_pot = v/static_cast<double>(npart); //Potential energy
-    stima_kin = t/static_cast<double>(npart); //Kinetic energy
-    stima_temp = (2.0 / 3.0) * t/static_cast<double>(npart); //Temperature
-    stima_etot = (t+v)/static_cast<double>(npart); //Total enery
+    stima_pot = v/static_cast<double>(n_particles); //Potential energy
+    stima_kin = t/static_cast<double>(n_particles); //Kinetic energy
+    stima_temp = (2.0 / 3.0) * t/static_cast<double>(n_particles); //Temperature
+    stima_etot = (t+v)/static_cast<double>(n_particles); //Total enery
 
     Epot << stima_pot  << std::endl;
     Ekin << stima_kin  << std::endl;
@@ -213,9 +378,9 @@ void molecular_dynamics_sim::write_config(const std::string & output_file) const
     std::cout << "Print final configuration to file " << output_file << "." << std::endl;
     std::ofstream output_file(output_file);
 
-    for(unsigned int i = 0; i < npart; ++i)
+    for(unsigned int i = 0; i < n_particles; ++i)
     {
-        output_file << x[i]/box << "   " <<  y[i]/box << "   " << z[i]/box << std::endl;
+        output_file << x[i]/cell_edge_length << "   " <<  y[i]/cell_edge_length << "   " << z[i]/cell_edge_length << std::endl;
     }
     output_file.close();
     return;
@@ -227,9 +392,9 @@ void molecular_dynamics_sim::write_config_xyz(const std::string & output_file, i
     std::ofstream output_file;
 
     output_file.open("frames/config_" + std::to_string(nconf) + ".xyz");
-    output_file << npart << std::endl;
+    output_file << n_particles << std::endl;
     output_file << "This is only a comment!" << std::endl;
-    for(unsigned int i = 0; i < npart; ++i)
+    for(unsigned int i = 0; i < n_particles; ++i)
     {
         output_file << "LJ  " << quotient(x[i]) << "   " <<  quotient(y[i]) << "   " << quotient(z[i]) << std::endl;
     }
@@ -238,5 +403,5 @@ void molecular_dynamics_sim::write_config_xyz(const std::string & output_file, i
 
 double molecular_dynamics_sim::quotient(double r) const
 {  
-    return r - box * std::round(r / box);
+    return r - cell_edge_length * std::round(r / cell_edge_length);
 }
--- exercise04/molecular_dynamics_sim.hh
+++ exercise04/molecular_dynamics_sim.hh
@@ -4,27 +4,31 @@
 class molecular_dynamics_sim
 {
     public:
-        void input(const std::string & input_filename);
+        void set_init_config(const std::string &);
         // Collect from a file the initial configuration of the system.
 
-        void initialise();
+        void initialise_uniform(const std::string &, Random & rng);
         // Generate an initial distribution of velocities, from which
         // simulate the position of the particles in the time step just
         // before the start.
-        // The velocities are drawn from a Maxwell-Boltzmann distribution
-        // in accord with the given temperature (this eliminates the need
-        // to rescale the velocities after they are generated).
+        // The velocities are drawn from a uniform distribution, then
+        // they are rescaled in order to match them with a given temperature.
         // Knowing the position at the current and previous steps, and the
         // acceleration exerted on each particle, the algorithm can start.
-        void initialise(const std::string &);
-        // This "pre-initial" configuration can be also be supplied as a
+        void initialise_maxwellboltzmann(const std::string &, Random & rng);
+        // Same as above, except this time the velocities are sampled from
+        // a Maxwell-Boltzmann distribution in accord with the given
+        // temperature (this eliminates the need to rescale the velocities
+        // afterwards).
+        void initialise_from_file(const std::string &, const std::string &);
+        // The "pre-initial" configuration can be also be supplied as a
         // text file; this way the position at the next step can be
         // calculated directly with the Verlet algorithm.
         // Then the velocity of the particles can be calculated, and it is
         // rescaled in order to match the previously set temperature.
 
         void move();
-        // Advance the algorithm one step at a time with the Verlet method.
+        // Advance the algorithm one step with the Verlet method.
 
         // Read some thermodynamic properties of the system, calculated from
         // its current state.
@@ -42,9 +46,9 @@ class molecular_dynamics_sim
         // system as the algorithm advances possible.
 
     private:
-        double force(unsigned int particle_index, unsigned int direction) const;
+        double force(unsigned int particle_index, unsigned int dir) const;
         // Calculate the force exerted on the particle at index
-        // 'particle_index', in the direction 'direction'.
+        // 'particle_index', in the direction 'dir'.
 
         double quotient(double) const;
         // Quotient the position/distance/etc. in order to reduce everything
@@ -52,24 +56,31 @@ class molecular_dynamics_sim
         // This imposes periodic boundary conditions on the unit cell of
         // the lattice.
 
-        // Internal parameters
-        double stima_pot, stima_kin, stima_etot, stima_temp;
+        std::vector<std::vector<double>> position, old_position, velocity;
+        // Position and veolcity of the particles: each sub-array is a
+        // triplet containing the position/velocity of the n-th particle.
+        // Another array contains the position of the particles at the
+        // previous integration step.
 
-        // averages
-        double acc,att;
+        // Internal / initial parameters
+        unsigned int n_particles, // Total number of particles in the system.
+                     n_steps,     // Number of integration steps.
+                     print_steps;
+        // In order not to use too many resources, the thermodynamical
+        // properties of the system are not evaluated at each new step, but
+        // only after 'print_steps' steps.
+        double input_temperature,
+               total_volume,
+               time_step,
+               particle_density,
+               cell_edge_length,
+               distance_cutoff;
 
-        //configuration
-        const int m_part=108;
-        double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
-        double vx[m_part],vy[m_part],vz[m_part];
-
-        // thermodynamical state
-        int npart;
-        double energy,temp,vol,rho,box,rcut;
-
-        // simulation
-        int nstep, iprint, seed;
-        double delta;
+        // ??
+        double potential_energy,
+               kinetic_energy,
+               total_energy,
+               temperature;
 
 };
 
