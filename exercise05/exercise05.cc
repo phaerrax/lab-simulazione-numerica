8b0b1e76617cf799d7e5b3e2dfb4af6833b739d6
Davide Ferracin <dv.ferracin@gmail.com>
2019-04-13 00:31:09 +0200

    [5] Complete plots of exercise 5
    The plots referring to the other distribution, the 2p state probability
    density, were calculated and added to the notebook.

--- exercise05/exercise05.cc
+++ exercise05/exercise05.cc
@@ -48,10 +48,12 @@ int main()
 
     const unsigned int dim = 3;
     double L(1.2);
-    metropolis_uniform<dim> metro(-L, L);
+    metropolis_uniform<dim> metro1s(-L, L);
+    metropolis_uniform<dim> metro2p(-L, L);
     
     std::array<double, dim> start = {1, 0, 0};
-    metro.set_starting_point(start);
+    metro1s.set_starting_point(start);
+    metro2p.set_starting_point(start);
 
     auto radius = [](const std::array<double, dim> & x)
     {
@@ -60,54 +62,78 @@ int main()
                 );
     };
 
-    std::function<double (std::array<double, dim>)> f = [radius](std::array<double, dim> x)
+    std::function<double (std::array<double, dim>)> f1s = [radius](std::array<double, dim> x)
     {
         return std::exp(-2. * radius(x)) / M_PI;
     };
-    // The function to be sampled.
+    std::function<double (std::array<double, dim>)> f2p = [radius](std::array<double, dim> x)
+    {
+        return std::pow(x[2], 2) * std::exp(-radius(x)) / (32 * M_PI);
+    };
+    // The functions to be sampled.
 
-    unsigned int n_steps(1e5);
-    std::vector<std::array<double, dim>> sequence;
+    unsigned int n_steps(1e6);
+    std::vector<std::array<double, dim>> sequence1s, sequence2p;
     std::array<double, dim> new_point;
-    std::vector<double> r;
+    std::vector<double> r1s, r2p;
     for(unsigned int n = 0; n < n_steps; ++n)
     {
-        new_point = metro.step(f, rng);
-        sequence.push_back(new_point);
-        r.push_back(radius(new_point));
+        new_point = metro1s.step(f1s, rng);
+        sequence1s.push_back(new_point);
+        r1s.push_back(radius(new_point));
+
+        new_point = metro2p.step(f2p, rng);
+        sequence2p.push_back(new_point);
+        r2p.push_back(radius(new_point));
     }
 
-    std::cerr << "Acceptance rate after " << n_steps << " steps: " << metro.get_acceptance_rate() << "." << std::endl;
+    std::cerr << "[1s] Acceptance rate after " << n_steps << " steps: " << metro1s.get_acceptance_rate() << "." << std::endl;
+    std::cerr << "[2p] Acceptance rate after " << n_steps << " steps: " << metro2p.get_acceptance_rate() << "." << std::endl;
 
     // Output the sequence of sampled points to a file.
-    std::ofstream output_file("sampled_points_uniform.dat");
+    std::ofstream output_file("1s_sampled_points_uniform.dat");
 
     const unsigned int col_width = 16;
     output_file.precision(4);
     output_file << std::scientific;
 
-    for(auto row : sequence)
+    for(auto row : sequence1s)
     {
         for(auto x = row.begin(); x != row.end(); ++x)
             output_file << std::setw(col_width) << *x;
         output_file << "\n";
     }
     output_file << std::endl;
+    output_file.close();
 
+    output_file.open("2p_sampled_points_uniform.dat");
+    for(auto row : sequence2p)
+    {
+        for(auto x = row.begin(); x != row.end(); ++x)
+            output_file << std::setw(col_width) << *x;
+        output_file << "\n";
+    }
+    output_file << std::endl;
     output_file.close();
 
     // Output the progressive values of the average radius and its standard
     // deviation, obtained with a blocking technique, to a file.
-    output_file.open("radius_avg_uniform.dat");
-
-    std::vector<std::vector<double>> r_blocks(block_statistics(r, 100));
-
-    for(auto row : r_blocks)
+    output_file.open("1s_radius_avg_uniform.dat");
+    std::vector<std::vector<double>> r1s_blocks(block_statistics(r1s, 100));
+    for(auto row : r1s_blocks)
         output_file << std::setw(col_width) << row[0]
                     << std::setw(col_width) << row[1]
                     << "\n";
     output_file << std::endl;
+    output_file.close();
 
+    output_file.open("2p_radius_avg_uniform.dat");
+    std::vector<std::vector<double>> r2p_blocks(block_statistics(r2p, 100));
+    for(auto row : r2p_blocks)
+        output_file << std::setw(col_width) << row[0]
+                    << std::setw(col_width) << row[1]
+                    << "\n";
+    output_file << std::endl;
     output_file.close();
 
     return 0;
