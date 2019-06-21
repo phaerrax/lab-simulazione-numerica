2b0ed34437693ce5e6f39bdc59683aa0be0d3293
Davide Ferracin <dv.ferracin@gmail.com>
2019-06-21 12:04:02 +0200

    Adapt "random" class files to coding style
    The code in the "random" class definition files was rearranged in order
    to fit with the coding style (spacing, indentation, and so on, but I
    left the names of the old methods untouched) I adopted throughout the
    rest of the project.

--- include/random.hh
+++ include/random.hh
@@ -11,26 +11,21 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 #ifndef __Random__
 #define __Random__
 
-class Random {
-
-private:
-  int m1,m2,m3,m4,l1,l2,l3,l4,n1,n2,n3,n4;
-
-protected:
-
-public:
-  // constructors
-  Random();
-  // destructor
-  ~Random();
-  // methods
-  void SetRandom(int * , int, int);
-  void SaveSeed();
-  double Rannyu(void);
-  double Rannyu(double min, double max);
-  double Gauss(double mean, double sigma);
+class Random
+{
+    public:
+        void SetRandom(int *, int, int);
+        void SaveSeed();
+        double Rannyu(void);
+        double Rannyu(double min, double max);
+        double Gauss(double mean, double sigma);
         double exponential(double rate);
         double cauchylorentz(double median, double scale);
+
+    private:
+        int m1, m2, m3, m4,
+            l1, l2, l3, l4,
+            n1, n2, n3, n4;
 };
 
 #endif // __Random__
