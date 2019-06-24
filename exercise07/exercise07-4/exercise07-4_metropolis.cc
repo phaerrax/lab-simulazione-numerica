a74fe36f6a7ec1067d8bb24dc6e67588b207c257
Davide Ferracin <dv.ferracin@gmail.com>
2019-06-24 18:39:27 +0200

    [7] Join exercise 7.2, 7.3 and 7.4
    Exercises 7.2 and 7.3 require no plots to be shown, just the addition of
    some functions to the code. Since these function are present in the
    programs of exercise 7.4 anyway, there is no need to have a separate
    directory for exercises 7.2 and 7.3.
    For this reason, those two directories were deleted, and their files
    moved together with the exercise 7.4 code.
    
    The input.dat files were edited so that they could be used with both
    _metropolis and _verlet programs (_metropolis needs one line more that
    tells the equilibration steps, that can be ignored by _verlet).

--- exercise07/exercise07-4/exercise07-4_metropolis.cc
+++ exercise07/exercise07-4/exercise07-4_metropolis.cc
@@ -48,22 +48,31 @@ int main(int argc, char *argv[])
     // Initialisation procedure
     // ========================
     // Get the name of the input file from the command line
-    if(argc != 2)
+    if(argc != 3)
     {
+        std::cerr << "Error: invalid input.\n"
+				  << "Syntax: " << argv[0] << " <element> <phase>\n"
+				  << "to use the parameters in the file \"./<element>/<phase>/input.dat\"." << std::endl;
         std::cerr << "Error: too few arguments." << std::endl;
         return 1;
     }
+
 	std::string particle_type(argv[1]),
-                input_parameters_file("input." + particle_type);
+				phase(argv[2]);
+
+    // The directory in which the input parameters and output files are
+    // stored.
+    std::string prefix = particle_type + "/" + phase + "/";
 
     std::cout << "Classic Lennard-Jones fluid: "
-              << particle_type << "\n"
+              << particle_type << " in a " << phase << " phase.\n"
               << "Molecular dynamics simulation in NVT ensemble\n\n"
               << "Interatomic potential V(r) = 4 * [(1/r)^12 - (1/r)^6]\n\n"
               << "The program uses Lennard-Jones units." << std::endl;
 
     // Read the input parameters from a file.
     // They have to be given in a very specific way...
+    std::string input_parameters_file(prefix + "input.dat");
     std::cout << "Read input parameters from " << input_parameters_file << "." << std::endl;
     std::ifstream input_parameters(input_parameters_file);
 
@@ -73,7 +82,8 @@ int main(int argc, char *argv[])
            distance_cutoff,
 		   step_stdev;
     unsigned int n_steps,
-				 n_equilibration_steps;
+				 n_equilibration_steps,
+				 block_size;
 
     input_parameters >> temperature
                      >> n_particles
@@ -81,6 +91,7 @@ int main(int argc, char *argv[])
                      >> distance_cutoff
 					 >> step_stdev
                      >> n_steps
+					 >> block_size
 					 >> n_equilibration_steps;
 
     input_parameters.close();
@@ -131,15 +142,9 @@ int main(int argc, char *argv[])
 
     // Simulation
     // ==========
-    //std::ofstream output(particle_type + "_output.dat");
-	std::ofstream avg_rd_output(particle_type + "_output.gofr.0"),
-	              final_rd_output(particle_type + "_output.gave.0"),
-	              other_output(particle_type + "_output.0");
-
     unsigned int n_conf(1),
-				 snapshot_steps(10);
-
-	unsigned int n_bins(100); // of the radial dist function.
+				 snapshot_steps(10),
+	             n_bins(100); // of the radial dist function.
 
     //std::vector<double> potential_en_density,
     //                    pressure;
@@ -174,6 +179,9 @@ int main(int argc, char *argv[])
         }
 
         progress = 100 * static_cast<double>(step) / n_steps;
+		/*
+		   I have to disable this part since the files it generates
+		   quickly eat up all the available disk space.
         if(step % snapshot_steps == 0)
         {
             // The integer n_conf distinguishes between the "snapshots"
@@ -184,6 +192,7 @@ int main(int argc, char *argv[])
             dynamo.write_config_xyz("frames/config_" + std::to_string(n_conf) + ".xyz");
             ++n_conf;
         }
+		*/
 
 		auto results = dynamo.measure(n_bins, max_distance_rd);
         potential_en_density.push_back(std::get<0>(results));
@@ -195,6 +204,54 @@ int main(int argc, char *argv[])
 
     dynamo.write_config("config.final");
 
+    // Calculate average values and standard deviation of the measured
+    // physical quantities.
+    std::vector<double> potential_en_density_avg,
+                        potential_en_density_std,
+                        pressure_avg,
+                        pressure_std;
+
+    block_statistics(
+            std::begin(potential_en_density),
+            std::end(potential_en_density),
+            std::back_inserter(potential_en_density_avg),
+            std::back_inserter(potential_en_density_std),
+            block_size
+            );
+
+    block_statistics(
+            std::begin(pressure),
+            std::end(pressure),
+            std::back_inserter(pressure_avg),
+            std::back_inserter(pressure_std),
+            block_size
+            );
+
+	const std::string suffix("_metropolis.dat");
+	std::ofstream avg_rd_output(prefix + "avg_radial_dist" + suffix),
+	              final_rd_output(prefix + "histogram_radial_dist" + suffix),
+                  pot_en_output(prefix + "pot_energy" + suffix),
+                  pressure_output(prefix + "pressure" + suffix);
+	pot_en_output      << "steps pot_en_avg pot_en_std\n";
+	pressure_output    << "steps pressure_avg pressure_std\n";
+	unsigned int steps;
+    for(unsigned int j = 0; j < potential_en_density_avg.size(); ++j)
+    {
+        steps = (j + 1) * block_size;
+		pot_en_output      << steps                       << " "
+						   << potential_en_density_avg[j] << " "
+					       << potential_en_density_std[j] << "\n";
+    }
+    pot_en_output.close();
+    for(unsigned int j = 0; j < pressure.size(); ++j)
+    {
+        steps = (j + 1) * block_size;
+        pressure_output    << steps                       << " "
+			               << pressure_avg[j]             << " "
+                           << pressure_std[j]             << "\n";
+    }
+    pressure_output.close();
+
 	// Calculate the average radial distribution with a blocking technique.
 	// Instead of summing directly each vector...
 	// ... transpose the "matrix", obtaining a vector of subvectors, each
@@ -202,7 +259,7 @@ int main(int argc, char *argv[])
 	// Then, with the results of the average and the standard deviation,
 	// build a vector back.
 	std::vector<std::vector<double>> tr_radial_distribution(
-			radial_distribution.begin()->size()
+			radial_distribution[0].size()
 			);
 	// radial_distribution.size() == n_steps,
 	// radial_distribution[0].size() == n_bins.
@@ -216,26 +273,27 @@ int main(int argc, char *argv[])
 			tr_radial_distribution[i][j] = radial_distribution[j][i];
 
 	// Call block_statistics on each subvector of tr_radial_distribution,
-	// that is on each "column" or radial_distribution.
-	std::vector<double> rd_final_stdev(tr_radial_distribution.size());
-	std::vector<std::vector<double>> rd_averages(tr_radial_distribution.size());
+	// that is on each "column" of radial_distribution.
+	std::vector<double> rd_final_stdev;
+	std::vector<std::vector<double>> rd_averages;
 
-	for(unsigned int i = 0; i < tr_radial_distribution.size(); ++i)
+	for(auto & row : tr_radial_distribution)
 	{
-        std::vector<double> err;
+        std::vector<double> avg, err;
 		block_statistics(
-                std::begin(tr_radial_distribution[i]),
-                std::end(tr_radial_distribution[i]),
-                std::back_inserter(rd_averages[i]),
+                std::begin(row),
+                std::end(row),
+                std::back_inserter(avg),
                 std::back_inserter(err),
-                tr_radial_distribution[i].size() / 100
+				block_size
                 );
 		// I am only interested in the averages and in the final stdev.
 
-        // rd_averages[i] will be a n_blocks-long vector; its elements
+        // avg will be a n_blocks-long vector; its elements
         // are the block averages of the i-th bin.
         // rd_averages itself has n_bins elements.
-		rd_final_stdev[i] = err.back();
+		rd_averages.push_back(std::move(avg));
+		rd_final_stdev.push_back(err.back());
 	}
 
 	// Output the block averages of the radial distribution function.
@@ -245,17 +303,19 @@ int main(int argc, char *argv[])
 	avg_rd_output << "\n";
 	// ...then each row that follows contains the average, block by
 	// block, of each bin.
-	for(unsigned int i = 0; i < rd_averages.size(); ++i)
+	for(unsigned int j = 0; j < rd_averages[0].size(); ++j)
 	{
 		// Each element in rd_averages is a histogram, that is the average
 		// histogram for that block.
 		// rd_averages[j][i] is the average value of the j-th bin for the
 		// i-th block.
-		for(unsigned int j = 0; j < rd_averages[0].size(); ++j)
+		// rd_averages.size() == n_bins,
+		// rd_averages[0].size() == n_blocks (whatever that is).
+		for(unsigned int i = 0; i < rd_averages.size(); ++i)
 			// Each element in row is the value in a bin.
 			// i = 0, ..., n_bins;
 			// j = 0, ..., n_blocks.
-			avg_rd_output << rd_averages[j][i] << "  ";
+			avg_rd_output << rd_averages[i][j] << "  ";
 		avg_rd_output << "\n";
 	}
 	avg_rd_output.close();
@@ -264,20 +324,15 @@ int main(int argc, char *argv[])
 	// The first column contains the upper bound of each bin.
 	final_rd_output << "bin_upper_bound final_rd_avg final_rd_std\n";
     for(unsigned int j = 0; j < rd_final_stdev.size(); ++j)
+	{
 		final_rd_output << bin_upper_bounds[j] << " "
 			            << rd_averages[j].back() << " "
 		  	            << rd_final_stdev[j] << "\n";
+	}
 	final_rd_output.close();
-
-	other_output << "potential_en_density pressure\n";
-    for(unsigned int j = 0; j < potential_en_density.size(); ++j)
-    {
-        other_output << potential_en_density[j] << " "
-                     << pressure[j]             << "\n";
-    }
-    other_output.close();
 
     std::cerr << "Acceptance rate: " << dynamo.get_acceptance_rate() << std::endl;
 
+	rng.SaveSeed();
     return 0;
 }
