7061ff40e49e81d0f886f8b17f1a17b3394f2f25
Davide Ferracin <dv.ferracin@gmail.com>
2019-07-01 17:17:59 +0200

    [7] Make run_all script more granular
    The script was redesigned in order to allow running only the verlet or
    the metropolis programs (once for each phase), so that one does not have
    to run everything if only one of the two programs has changed.

--- exercise07/exercise07-4/run_all.sh
+++ exercise07/exercise07-4/run_all.sh
@@ -3,25 +3,53 @@
 phases=("solid" "liquid" "gaseous")
 exe="./exercise07-4"
 
-echo "Cleaning up old frame files..."
-for phase in ${phases[@]}
-do
-	# Cleanup...
-	frames_dir=$phase/frames
-	if [ -d $frames_dir ]
-	then
-		rm -f $frames_dir/*
-	else
-		mkdir $frames_dir
-	fi
-done
+cleanup()
+{
+	echo "Cleaning up old frame files..."
+	for phase in ${phases[@]}
+	do
+		# Cleanup...
+		frames_dir=$phase/frames
+		if [ -d $frames_dir ]
+		then
+			rm -f $frames_dir/*
+		else
+			mkdir $frames_dir
+		fi
+	done
+}
 
-for phase in ${phases[@]}
-do
-	# The programs write on and read from some config.*
-	# files, so it is not safe to launch all of them
-	# simultaneously.
-	${exe}_verlet ${phase}
-	${exe}_metropolis ${phase}
-	echo "Simulation of $phase phase complete."
-done
+run()
+{
+	for phase in ${phases[@]}
+	do
+		${exe}_$1 ${phase}
+		echo "Simulation of $phase phase complete."
+	done
+}
+
+# The programs write on and read from some config.*
+# files, so it is not safe to launch all of them
+# simultaneously.
+if [ $# -eq 0 ]
+then
+	cleanup
+	run verlet
+	run metropolis
+elif [ $# -eq 1 ]
+then
+	if [ $1 == "verlet" ]
+	then
+		cleanup
+		run verlet
+	elif [ $1 == "metropolis" ]
+	then
+		cleanup
+		run metropolis
+	else
+		echo "Invalid input."
+	fi
+else
+	echo "Invalid input."
+fi
+
